a98d74e58ec7072a4f081515adf9013b349a4338
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2021-04-11 07:14:48 +0200

    success

--- main.py
+++ main.py
@@ -10,28 +10,28 @@ from flask_cors import CORS
 
 
 
-# def gen(camera):
+def gen(camera):
 
-#     while True:
-#         frame=camera.get_frame()
-#         yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n'+ frame
-#                + b'Content-Type: \r\n\r\n'
-#                + b'\r\n\r\n')
+    while True:
+        frame=camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n'+ frame
+               + b'Content-Type: \r\n\r\n'
+               + b'\r\n\r\n')
 
 app=Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['GET'])
 def first():
-   return jsonify({'message': 'success root'})
-    # return render_template('detect.html')
+#    return jsonify({'message': 'success root'})
+    return render_template('detect.html')
 
 
 @app.route('/video_feed')
 def video_feed():
-    return jsonify({'message': 'success video feed'})
-    # return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # return jsonify({'message': 'success video feed'})
+    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
