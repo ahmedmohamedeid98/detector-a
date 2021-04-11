e0bd5d9eb0fb03ddc36b7f963c38b3616a8f6e85
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2021-04-11 06:44:02 +0200

    logger

--- main.py
+++ main.py
@@ -7,28 +7,34 @@ from flask import Flask,jsonify,render_template,Response, request,redirect, url_
 
 from flask_cors import CORS 
 
+
+
+# def gen(camera):
+
+#     while True:
+#         frame=camera.get_frame()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n'+ frame
+#                + b'Content-Type: \r\n\r\n'
+#                + b'\r\n\r\n')
+
 app=Flask(__name__)
 CORS(app)
-
-def gen(camera):
-
-    while True:
-        frame=camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n'+ frame
-               + b'Content-Type: \r\n\r\n'
-               + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return jsonify({'message': 'success video feed'})
-    # return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/', methods=['GET'])
 def First():
    return jsonify({'message': 'success root'})
     # return render_template('detect.html')
 
+    
+@app.route('/video_feed')
+def video_feed():
+    return jsonify({'message': 'success video feed'})
+    # return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 if __name__=="__main__":
     app.run(debug=True)
